d93ccf7f337e653f4c23af5152e90089a4e73ab8
Nicolas GUILLOUZOUIC - CDA Compte d'administration LOCAL <66hAdminLOCAL@casn.net>
2026-09-18 11:13:53 +0200

    installpdfparse

--- server.js
+++ server.js
@@ -154,14 +154,30 @@ app.get("/classement/:equipe", async (req, res) => {
 
 app.get(
   "/test-pdf/:fdmCode",
-  async (req, res) => {
+  async (req, res      req.params.fdmCode;
 
-    const url =
-      `https://fdm.fdme.ffhandball.fr/${code[0]}/${code[1]}/${code[2]}/${code[3]}/${code}.pdf`;
+      const url =
+        `https://fdm.fdme.ffhandball.fr/${code[0]}/${code[1]}/${code[2]}/${code[3]}/${code}.pdf`;
 
-    res.json({
-      url
-    });
+      console.log(
+        "URL PDF =",
+        url
+      );
+
+      res.json({
+        fdmCode: code,
+        url
+      });
+
+    } catch (err) {
+
+      console.error(err);
+
+      res.status(500).json({
+        erreur: err.message
+      });
+
+    }
 
   }
 );
